65a90d298c25586db3e49b8bdd267edb6a3c3bd5
Zushisan <74838@supinfo.com>
2018-01-02 15:17:46 +0000

    Queen threat final first version

--- queen-threat.js
+++ queen-threat.js
@@ -8,6 +8,7 @@ var queenThreat = function(queen1, queen2){
   //var row = col;
   //var row = [1, 2, 3, 4, 5, 6, 7, 8];
 
+  // Chessboard display -----------------------------------------------------------------
   for(var i = 0; i < col.length; i++){
     for(var j = 0; j < col.length; j++){
 
@@ -20,15 +21,34 @@ var queenThreat = function(queen1, queen2){
       }
 
     }
-
     chessboard = chessboard.concat("\n");
   }
 
   console.log(chessboard);
+  // ------------------------------------------------------------------------------------
 
+  var result = new Boolean(false);
+
+  // First we check if the Queens are on the same row or column
+  if( queen1[0] == queen2[0] || queen1[1] == queen2[1]){
+    result = true;
+  }
+
+  // For the diagonal we are only gonna compare queen1 diagonal to queen2 position
+  // The possibilities are x+n,y+n or x-n,y-n or x+n,y-n or x-n,y+n
+  for(k = 0; k < col.length; k++){
+
+    if((queen1[0] + k == queen2[0] && queen1[1] + k == queen2[1]) ||
+       (queen1[0] - k == queen2[0] && queen1[1] - k == queen2[1]) ||
+       (queen1[0] + k == queen2[0] && queen1[1] - k == queen2[1]) ||
+       (queen1[0] - k == queen2[0] && queen1[1] + k == queen2[1])){
+
+      result = true;
+    }
+  }
+
+    console.log(result+"\n");
 }
 
 queenThreat([0, 5], [5, 0]);
 queenThreat([0, 0], [7, 5]);
-
-
